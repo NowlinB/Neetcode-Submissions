fe62c9c0c5ec8941cd4125484f0fd04893f9cbc2
Brandon Nowlin <38094031+NowlinB@users.noreply.github.com>
2024-06-22 13:21:52 -0500

    Add files via upload

---
+++ Medium/group_anagrams.py
@@ -0,0 +1,72 @@
+"""
+Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+"""
+
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        i, j, = 0, 1
+        print(len(strs))
+        if (len(strs) < 2):
+            return [strs]
+        inAnagram = False
+        mySolution = Solution()
+        anagrams = []
+        while i != len(strs):
+            j = 0
+            tempList = []
+            for sublist in strs:
+                if (j == len(strs)):
+                    break
+                firstIndex = strs.index(strs[i])
+                secondIndex = strs.index(strs[j])
+                first = strs[firstIndex]
+                second = strs[secondIndex]
+                # check if current first and second element are valid anagrams of each other
+                result = mySolution.isAnagram(first, second)
+                if (result):
+                    inAnagram = mySolution.checkInAnagramList(first, anagrams)
+                    # if two elements are exactly the same they are anagrams of each other
+                    if (first == second and not inAnagram):
+                        tempList.append(first)
+                    if (not inAnagram):
+                        inTempList = mySolution.checkInAnagramList(
+                            first, tempList)
+                        if not inTempList:
+                            tempList.append(first)
+                    inAnagram = mySolution.checkInAnagramList(second, anagrams)
+                    if not inAnagram:
+                        inTempList = mySolution.checkInAnagramList(
+                            second, tempList)
+                        if not inTempList:
+                            tempList.append(second)
+                j += 1
+            if (len(tempList) > 0):
+                anagrams.append(tempList)
+            i += 1
+        return anagrams
+
+    # used to check if two strings are valid anagrams of each other
+    def isAnagram(self, first: str, second: str) -> bool:
+        i = 0
+        if len(first) != len(second):
+            return False
+        for item in first:
+            if (first[i] not in second or first.count(first[i]) != second.count(first[i])):
+                return False
+            i += 1
+        return True
+
+    # used to check if a given anagram already exists in the list of anagrams
+    def checkInAnagramList(self, first: str, anagrams: list) -> bool:
+        for sublist in anagrams:
+            if (first in sublist):
+                return True
+        return False
+
+
+testSolution = Solution()
+testStrings = ["buy", "sid", "dis", "sid", "buy"]
+checkAnagram = testSolution.groupAnagrams(testStrings)
+print(checkAnagram)
