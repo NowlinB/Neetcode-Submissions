30da4a275124665cb656be54fd6aa6759fb206cd
Brandon Nowlin <38094031+NowlinB@users.noreply.github.com>
2024-06-23 11:12:47 -0500

    Update encode_and_decode_strings.py

--- Medium/encode_and_decode_strings.py
+++ Medium/encode_and_decode_strings.py
@@ -1,8 +1,5 @@
 class Solution:
-    """
-    Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
-    """
-
+    """Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings."""
     def encode(self, strs: list[str]) -> str:
         i = 0  # used as a counter to determine when we have reached the last element of the list
         encoded_string = ""
