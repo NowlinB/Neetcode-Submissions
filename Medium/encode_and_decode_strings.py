be7bcdcbcf3fcc4e30c3ad11153af3128ee86ceb
Brandon Nowlin <38094031+NowlinB@users.noreply.github.com>
2024-06-23 11:09:27 -0500

    Add files via upload

---
+++ Medium/encode_and_decode_strings.py
@@ -0,0 +1,31 @@
+class Solution:
+    def encode(self, strs: list[str]) -> str:
+        sol = Solution()
+        i = 0  # used as a counter to determine when we have reached the last element of the list
+        encoded_string = ""
+        if (len(strs) == 0):
+            return strs
+        for s in strs:
+            # we know that we've reached the last element if i == (len(strs) - 1)
+            if (i == (len(strs)-1)):  # prevent adding an additional empty '' element into our list
+                encoded_string += s
+            else:
+                encoded_string += s + "-"  # I am using the "-" as a delimiter
+            i += 1
+        return encoded_string
+
+    def decode(self, s: str) -> list[str]:
+        strs = []
+        if (s == ""):  # if the string contains only an empty string we still want to return a list containing the empty string
+            strs.append("")
+        if (len(s) > 0):  # otherwise split the string into a list by the "-" delimiter
+            strs = s.split("-")
+        return strs
+
+
+testSolution = Solution()
+testList = [""]
+testEncode = testSolution.encode(testList)
+testDecode = testSolution.decode(testEncode)
+print(testEncode)
+print(testDecode)
