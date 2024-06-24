f423ad0c15908183f10182d920854a1af08a5dca
NowlinB <brandon_nowlin@protonmail.com>
2024-06-24 13:50:31 -0500

    test

---
+++ Medium/product_of_array_except_self.py
@@ -0,0 +1,27 @@
+"""
+Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+Each product is guaranteed to fit in a 32-bit integer. 
+"""
+
+
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        i, j = 0, 0
+        product = 1  # stores the current product
+        products = []  # stores the list of products
+        while i < len(nums):
+            j = 0
+            product = 1
+            while j < len(nums):
+                if j != i:
+                    product *= nums[j]
+                j += 1
+            i += 1
+            products.append(product)
+        return products
+
+
+sol = Solution()
+nums = [-1, 0, 1, 2, 3]
+testProduct = sol.productExceptSelf(nums)
+print(testProduct)
