0bfe9723d170684735db8cdfcc0fd956ce838da0
Brandon Nowlin <38094031+NowlinB@users.noreply.github.com>
2024-07-27 23:15:23 -0500

    Update min_stack.py

--- Medium/min_stack.py
+++ Medium/min_stack.py
@@ -4,37 +4,43 @@
     void push(int val) pushes the element val onto the stack.
     void pop() removes the element on the top of the stack.
     int top() gets the top element of the stack.
-    int getMin() retrieves the minimum element in the stack"""
+    int getMin() retrieves the minimum element in the stack
+    Each function should run in O(1) time."""
 
 
 class MinStack:
 
     def __init__(self):
         self.stack = []
+        self.min = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        print(self.stack)
+        if len(self.min) == 0:
+            self.min.append(val)
+        else:
+            self.min.append(min(val, self.min[-1]))
 
     def pop(self) -> None:
         self.stack.pop()
+        self.min.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        min = self.stack[0]  # initialize
-        for i in range(0, len(self.stack)):
-            if self.stack[i] < min:
-                min = self.stack[i]
-        return min
+        if len(self.min) > 0:
+            return self.min[-1]
+        else:
+            return None
 
 
 minStack = MinStack()
-minStack.push(1)
-minStack.push(2)
-minStack.push(0)
-print(minStack.getMin())  # return 0
-minStack.pop()
-print(minStack.top())  # return 2
+print(minStack.push(1))
+print(minStack.push(2))
+print(minStack.push(0))
+# print(minStack.push(4))
+print(minStack.getMin())
+print(minStack.pop())
+print(minStack.top())
 print(minStack.getMin())
